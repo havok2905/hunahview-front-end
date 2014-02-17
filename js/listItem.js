82b351b1e7e4529db66ebd0ba4ddbbb3a2597a2a
Chris McLean <chrismclean@Cloudspaces-MacBook-Pro.local>
2014-02-17 16:29:24 -0500

    Added the skeleton for a list item module

---
+++ js/listItem.js
@@ -0,0 +1,31 @@
+listItem = function(elements)
+{
+	publicJqueryElements = elements;
+
+	publicRegisterEvents = function(publicJqueryElements)
+	{
+		
+	};
+
+	privateHandleAnchor = function(anchor)
+	{
+		switch(anchor.tag)
+		{
+			case : "city"
+				break;
+			case : "state"
+				break;
+			case : "country"
+				break;
+			case : "brewery"
+				break;
+			default
+				break;
+		}
+	};
+
+	return {
+		jqueryElements : publicJqueryElements,
+		registerEvents : publicRegisterEvents
+	}
+}();
