3eaca0a8e4d43cc5d6b34c8bc20bf8e33cadbde7
Tom McLean <tmclean@feith.com>
2014-02-19 01:47:11 -0500

    Merging api

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function()
 {
+	$body = $("body");
+
+	$(document).on({
+    	ajaxStart: function() { $body.addClass("loading");    },
+    	ajaxStop: function() { $body.removeClass("loading"); }    
+	});
+	
 	buttons.registerEventListeners();
 });
