5b63d63031ccca61149f0c1aa952bc6d412183f6
Chris McLean <chrismclean@Cloudspaces-MacBook-Pro.local>
2014-03-02 23:28:03 -0500

    sorting locations by state and city

--- js/locations.js
+++ js/locations.js
@@ -1,14 +1,44 @@
 var locationModel = (function()
 {
-	function publicPrintLocationsList(locations)
+	function publicPrintLocationsList()
 	{
-		api.getLocations(api.currentEvent, function( locations ){
+		api.getLocations(api.currentEvent, function( locations )
+		{
+			privateSortLocations(locations);
+
 			$("#response").html("<ul id='location-list'></ul>");
 			$.each(locations, function(index, location)
 			{
 				$("#location-list").append("<li><a data-tag='city' href='" + location.city + "'>" + location.city + " " + location.state + " " + location.country + "</a></li>");
 			});
 			listItem.registerEventListeners();
+		});
+	}
+
+	function privateSortLocations(locations)
+	{
+		locations.sort(function(x, y) 
+		{
+		   	if(x.state > y.state)
+		   	{       
+		    	return 1;
+		   	}
+		   	else if (x.state < y.state)
+		   	{
+		    	return -1;
+		   	}
+		   	else if (x.city > y.city)
+		   	{
+		    	return 1;
+		   	}
+		   	else if (x.city < y.city)
+		   	{
+		    	return -1;
+		   	}
+		   	else
+		   	{
+		    	return 0;
+		   	}
 		});
 	}
 
